5ad39a97d19541600f844ba878ab41cff58e7bc0
Vlad Rublenko <vladik.rar@ukr.net>
2025-08-18 20:30:28 +0300

    added new class-level variable and fixed return

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,9 +13,9 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        User user = new UserService().findByEmail(email);
+        UserService userService = new UserService();
+        User user = userService.findByEmail(email);
 
-        return new UserService().findByEmail(email) != null
-                && user.getPassword().equals(password);
+        return user != null && user.getPassword().equals(password);
     }
 }
